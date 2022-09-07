a86bf22e880536d59fa6a778e8ccc7b975da04a9
awaik <agoradeskdeus@gmail.com>
2022-09-07 12:02:21 +0300

    chore: ci for releases

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -4,6 +4,6 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-#gpg --quiet --batch --yes --decrypt --passphrase="$RELEASE_AGORADESK_KEYSTORE_SECRET" \
-#--output $HOME/my_secret.json $RELEASE_AGORADESK_KEYSTORE
-gpg -d --batch --passphrase $RELEASE_AGORADESK_KEYSTORE_SECRET $RELEASE_AGORADESK_KEYSTORE
+gpg --quiet --batch --yes --decrypt --passphrase="$RELEASE_AGORADESK_KEYSTORE_SECRET" \
+--output $HOME/my_secret.json $RELEASE_AGORADESK_KEYSTORE
+#gpg -d --batch --passphrase $RELEASE_AGORADESK_KEYSTORE_SECRET --output $HOME/my_secret.json $RELEASE_AGORADESK_KEYSTORE
