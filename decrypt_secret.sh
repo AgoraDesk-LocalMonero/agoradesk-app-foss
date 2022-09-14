c27add297381b1aa52fe5f26832522369deedee4
awaik <agoradeskdeus@gmail.com>
2022-09-14 11:45:15 +0300

    chore: ci settings

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "$RELEASE_AGORADESK_KEYSTORE" > keystoreIn.jks
+echo "$RELEASE_KEYSTORE" > keystoreIn.jks
 
-gpg -d --batch --passphrase $RELEASE_AGORADESK_KEYSTORE_SECRET keystoreIn.jks > releaseOut.jks
+gpg -d --batch --passphrase $RELEASE_KEYSTORE_SECRET keystoreIn.jks > releaseOut.jks
 cat releaseOut.jks
