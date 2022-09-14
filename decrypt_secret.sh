8247f5038aa12a6d158e4add8f60607860cc940c
awaik <agoradeskdeus@gmail.com>
2022-09-14 10:25:27 +0300

    chore: android manifest changed name

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-#echo $(ls)
-
 echo "$RELEASE_AGORADESK_KEYSTORE" > keystoreIn.jks
 
 gpg -d --batch --passphrase $RELEASE_AGORADESK_KEYSTORE_SECRET keystoreIn.jks > releaseOut.jks
 cat releaseOut.jks
-
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-#gpg --quiet --batch --yes --decrypt --passphrase="$RELEASE_AGORADESK_KEYSTORE_SECRET" \
-#--output $HOME/my_secret.json $RELEASE_AGORADESK_KEYSTORE
-
-#gpg -d --batch --passphrase $RELEASE_AGORADESK_KEYSTORE_SECRET --output $HOME/my_secret.json $RELEASE_AGORADESK_KEYSTORE
