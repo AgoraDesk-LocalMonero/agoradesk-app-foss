4288430261b6411376d7ebaf255bb86d2a3f6576
awaik <agoradeskdeus@gmail.com>
2022-12-25 17:02:49 +0100

    feat: #153 remove all badges

--- lib/features/wallet/models/notifications_view_model.dart
+++ lib/features/wallet/models/notifications_view_model.dart
@@ -12,6 +12,7 @@ import 'package:agoradesk/router.gr.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_badger/flutter_app_badger.dart';
 import 'package:vm/vm.dart';
 
 class NotificationsViewModel extends ViewModel with StringMixin, ValidatorMixin, ClipboardMixin, ErrorParseMixin {
@@ -117,7 +118,7 @@ class NotificationsViewModel extends ViewModel with StringMixin, ValidatorMixin,
       }
       notifications.clear();
       notifications.addAll(notificationsMarkedAsRead);
-      // AwesomeNotifications().dismissAllNotifications();
+      FlutterAppBadger.removeBadge();
       notifyListeners();
     } else {
       handleApiError(res.left, context);
